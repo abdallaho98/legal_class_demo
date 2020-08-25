7db1e7c8e1e0dd288bec87579f4028166192fd60
Abdallah Herzallah <fa_herzallah@esi.dz>
2020-08-25 06:27:42 +0100

    import axios

--- src/legal.vue
+++ src/legal.vue
@@ -1,18 +1,47 @@
 <template>
     <div class="legal-item">
-    <h2 class="text">{{legal}}</h2>
-    
+        <h2 class="text">{{legal}}</h2>
+        <center>
+            <div>
+                <div class="horiz"><vc-donut :size="120" :sections="sections0" :thickness="30"><h2>droit</h2><h1>{{this.sections0.value}}</h1></vc-donut></div>
+                <div class="horiz"><vc-donut :size="120" :sections="sections1" :thickness="30"><h2>permission</h2></vc-donut></div>
+                <div class="horiz"><vc-donut :size="120" :sections="sections2" :thickness="30"><h2>obligation</h2></vc-donut></div>
+                <div class="horiz"><vc-donut :size="120" :sections="sections3" :thickness="30"><h2>prohibition</h2></vc-donut></div>
+                <div class="horiz"><vc-donut :size="120" :sections="sections4" :thickness="30"><h2>autre</h2></vc-donut></div>
+            </div>
+        </center>
     </div>
 </template>
 
 <script>
 
-import ChartDoughnut from "@/components/chart-doughnut";
+import Donut from 'vue-css-donut-chart';
 
 export default {
     name:'item',
     props: ['legal'],
-    
+    components: [
+        Donut
+    ],
+    data () {
+      return {
+        sections0: [
+          { label: 'Blue section', value: 25, color: '#4d73bf' }
+        ] ,
+        sections1: [
+          { label: 'Blue section', value: 25, color: '#df8342' }
+        ] ,
+        sections2: [
+          { label: 'Blue section', value: 25, color: '#f6c343' }
+        ] ,
+        sections3: [
+          { label: 'Blue section', value: 25, color: '#a6a6a6' }
+        ] ,
+        sections4: [
+          { label: 'Blue section', value: 25, color: '#6a9cd1' }
+        ] ,
+      }
+    },
 }
 </script>
 
@@ -35,4 +64,11 @@ export default {
     .text{
         line-height: 32px;
     }
+
+    .horiz{
+        display: inline;
+        display: inline-block;
+        margin: 32px;
+    }
+
 </style>
